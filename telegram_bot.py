6b3ba1d5c985824ed617b227b259586f68d35210
irfanturkoz <turkozirfan43@gmail.com>
2025-07-31 21:38:33 +0300

    Fix syntax

--- telegram_bot.py
+++ telegram_bot.py
@@ -566,7 +566,7 @@ def perform_scan():
                     ma_trend = 'Güçlü Yükseliş'
                 elif df['MA7'].iloc[-1] < df['MA25'].iloc[-1] < df['MA50'].iloc[-1] < df['MA99'].iloc[-1]:
                     ma_trend = 'Güçlü Düşüş'
-            else:
+                else:
                     ma_trend = 'Kararsız'
                 
                 fibo_levels, fibo_high, fibo_low = calculate_fibonacci_levels(df)
